c49baaf2629613cd916c1971529a1ce6b0df2ab3
cledson-leite <csbetsonline@gmail.com>
2022-06-14 15:49:40 -0300

    refactorando para ter uma lista dinamica

--- src/componets/Lista/index.tsx
+++ src/componets/Lista/index.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 
 function Lista() {
+  const tarefas = [
+    {tarefa: "React", tempo: "02:00:00"},
+    {tarefa: "Javascript", tempo: "01:00:00"}
+  ]
   return (
     <aside>
       <h2>
         Estudos do dia
       </h2>
       <ul>
-        <li>
-          <h3>React</h3>
-          <span>02:00:00</span>
+        {tarefas.map((item, index) => (
+          <li key={index}>
+            <h3>{item.tarefa}</h3>
+          <span>{item.tempo}</span>
         </li>
-        <li>
-          <h3>javascript</h3>
-          <span>01:00:00</span>
-        </li>
+        ))}
       </ul>
     </aside>
   )
