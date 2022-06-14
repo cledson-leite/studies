22591d0559fd88646f071642d45a9123b25bc074
cledson-leite <csbetsonline@gmail.com>
2022-06-14 15:36:29 -0300

    implementação da lista de estudos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Formulario from './componets/Formulario';
+import Lista from './componets/Lista';
 
 function App() {
   return (
     <div className="App">
       <Formulario />
+      <Lista />
     </div>
   );
 }
