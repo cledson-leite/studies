23b63a2433ebc70057a6f156e342d70a8c8fb44c
cledson-leite <csbetsonline@gmail.com>
2022-06-14 16:36:35 -0300

    estilizando com sass

---
+++ src/App/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Formulario from '../componets/Formulario';
+import Lista from '../componets/Lista';
+
+import "./style.scss"
+
+function App() {
+  return (
+    <div className="AppStyle">
+      <Formulario />
+      <Lista />
+    </div>
+  );
+}
+
+export default App;
